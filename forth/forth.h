e5ff336ab7045cb0170f16c480ed92e20434c002
Kristopher Johnson <kris@kristopherjohnson.net>
2014-12-02 06:22:05 -0500

    Initial Commit

---
+++ forth/forth.h
@@ -0,0 +1,19 @@
+//
+//  forth.h
+//  forth
+//
+//  Created by Kristopher Johnson on 12/2/14.
+//  Copyright (c) 2014 Kristopher Johnson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for forth.
+FOUNDATION_EXPORT double forthVersionNumber;
+
+//! Project version string for forth.
+FOUNDATION_EXPORT const unsigned char forthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <forth/PublicHeader.h>
+
+
